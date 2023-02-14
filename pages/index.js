8e1a7f9d66c1b12a294a47c490333568b1688089
Kamve Gwijana <gwijgwij011@gmail.com>
2023-02-14 17:55:08 +0200

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import { createElement } from 'react'
+import Image from "next/image"
+
 export default function Home() {
   
   var id = null;
@@ -167,6 +169,17 @@ export default function Home() {
 
           <div id="work-xp">
             <h1>Work Experience</h1>
+            <p>
+            Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: 
+            a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. 
+            In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. 
+            A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). 
+            Length and appearance do not determine whether a section in a paper is a paragraph. 
+            For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. 
+            Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, 
+            we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.
+            </p>
+
             <div id="Farcast">
               <h2>Farcast</h2>
             </div>
